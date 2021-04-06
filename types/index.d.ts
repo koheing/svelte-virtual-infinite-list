e4c396f2eee984155f3fd5c9cde5a5a8c4fe2046
koheing <saandna111@gmail.com>
2021-04-06 23:15:43 +0900

    fix: change VirtualInfiniteListProps's type

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,24 +5,26 @@ export interface InfiniteEvent extends CustomEvent<{ on: 'top' | 'bottom' }> {}
 
 export interface VirtualInfiniteListProps {
   items: unknown[]
-  itemHeight: number | undefined
+  itemHeight?: number
   /**
+   * [**For direction-top infinite scroll user**]
    * Maximum number of items loaded per load.
    * The offset after loaded may be significantly shift
    * if the number of items that exceeds this value is loaded.
+   * `default value is 0.`
    */
-  maxItemCountPerLoad: number
+  maxItemCountPerLoad?: number
   loading: boolean
   direction: 'top' | 'bottom'
-  scrollTo: (offset: number) => void
+  scrollTo?: (offset: number) => void
   /**
    * read-only, but visible to consumers via bind:start
    */
-  readonly start: number
+  readonly start?: number
   /**
    * read-only, but visible to consumers via bind:end
    */
-  readonly end: number
+  readonly end?: number
 }
 
 export default class VirtualInfiniteList extends SvelteComponentTyped<
